ae6bfb91014854a5d780dc668300186565e03954
armagancaglar <c.armagancaglar@gmail.com>
2018-05-11 23:45:35 +0300

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -20,6 +20,17 @@ io.on('connection', (socket) => {
         createAt: 123
     });
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+    
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {
@@ -27,7 +38,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });        
-    });
+        //  socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // }); 
+});
     
     socket.on('disconnect', () => {
         console.log('User was disconnected');    
